d711bc76c0e2f79eaf0e143cbeb94fe5776ce119
Fakhri Ghribi <fakhrighribi3@gmail.com>
2022-02-03 17:27:45 +0100

    upload the intervlan project to github

---
+++ Test/testscript.sh
@@ -0,0 +1,5 @@
+ansible-playbook main_play_intervlan.yml --limit intervlan --verbose
+#python3 -m pytest Test/testmulticast.py --disable-warnings -s --verbose
+
+#ansible-playbook main-play-multicast.yml --limit multicat --verbose
+#python3 -m pytest Test/testmulticast.py --disable-warnings -s --verbose
