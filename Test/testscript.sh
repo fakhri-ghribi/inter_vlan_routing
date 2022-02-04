d6f3033742a02dd5fffd3ebc41763c16ef200800
Fakhri Ghribi <fakhrighribi3@gmail.com>
2022-02-04 01:19:42 +0100

    commit

--- Test/testscript.sh
+++ Test/testscript.sh
@@ -1,5 +1,5 @@
 ansible-playbook main_play_intervlan.yml --limit dist --verbose
-ansible-playbook main_play_access.yml --limit access --verbose
+ansible-playbook main_paly_access.yml --limit access --verbose
 #python3 -m pytest Test/testmulticast.py --disable-warnings -s --verbose
 
 #ansible-playbook main-play-multicast.yml --limit multicat --verbose
